9b085de2e4f43a5fa55ae48bbb9049233c870635
Reda Mohamed <RedaElhadidy@go.local>
2016-02-24 14:39:43 +0200

    first commit

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var messageTextField: UITextField!
+    @IBOutlet weak var sendMessageButton: UIButton!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +24,23 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    @IBAction func onSendMessageClicked(sender: UIButton) {
+        messageLabel.hidden = false
+        messageLabel.text? = sendMessage(nameTextField.text!, hasNewMessage: messageTextField.text!)
+        
+        
+        messageTextField.text = ""
+        messageTextField.resignFirstResponder()
+        nameTextField.text = ""
+        
+        
+        
+        sendMessageButton.setTitle("message Sent", forState: UIControlState.Normal)
+    }
+    
+    func sendMessage(name: String, hasNewMessage : String)->String{
+        return "\(name) has new message with content \(hasNewMessage)";
+    }
 
 }
 
